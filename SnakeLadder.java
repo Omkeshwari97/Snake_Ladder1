2ff91bc977aa7bde9ae7ecaac45b053d0d8bb5dc
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-27 20:20:15 +0530

    UC2 commit

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,5 +5,9 @@ public class SnakeLadder
 	public static void main(String args[])
 	{
 		int position=0;
+		//System.out.println(Math.random()*5);
+		int dieNumber=(int)(Math.random()*5)%7;
+		System.out.println("Number when die rolled: "+dieNumber);
+		
 	}
 }
